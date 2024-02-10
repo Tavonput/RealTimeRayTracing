c547b1dc4e7303e0df122937c156c853ef3e66a2
JohnnyFlint <115769806+JohnnyFlint@users.noreply.github.com>
2024-02-09 20:41:38 -0800

    initilize denoizer & setup function declerations

--- RayTrace/Src/Application/simpleDenoise.cpp
+++ RayTrace/Src/Application/simpleDenoise.cpp
@@ -4,7 +4,23 @@
 #include "NRD.h"
 #include "NRDDescs.h"
 
-int startDenoise(struct InstanceCreationDesc& creationDetails,struct instance& instanceinfo,struct CommonSettings& settings) {
 
-	return 0;
+void startDenoise(struct nrd::InstanceCreationDesc& creationDetails,  struct nrd::Instance* instanceinfo,struct nrd::CommonSettings& settings,bool debug) {
+	nrd::Instance* instancecopy = instanceinfo;
+	if (debug) {
+		settings.debug = 1;
+	}
+	//TODO: Fix error on compiling 
+	//CreateInstance(creationDetails, instancecopy);
+
+	//SetCommonSettings(*instancecopy,settings);
+
+}
+
+void endDenoise(struct nrd::Instance* instanceinfo) {
+	nrd::Instance* instancecopy = instanceinfo;
+	//DestroyInstance(*instancecopy);
+}
+void frameprep(int framePosition) {
+
 }
