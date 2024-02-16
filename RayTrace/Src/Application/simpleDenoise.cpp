54482e1e207137bce62c7910ff5cb7795e7506f4
JohnnyFlint <115769806+JohnnyFlint@users.noreply.github.com>
2024-02-16 12:41:01 -0800

    Update Prepframe function

--- RayTrace/Src/Application/simpleDenoise.cpp
+++ RayTrace/Src/Application/simpleDenoise.cpp
@@ -10,17 +10,16 @@ void startDenoise(struct nrd::InstanceCreationDesc& creationDetails,  struct nrd
 	if (debug) {
 		settings.debug = 1;
 	}
-	//TODO: Fix error on compiling 
-	//CreateInstance(creationDetails, instancecopy);
-
-	//SetCommonSettings(*instancecopy,settings);
-
+	//TODO: Fix repo to include more of NRD source files without having them on the realTimeRayTrace repo
+	 //nrd::CreateInstance(creationDetails, instancecopy);
+	 //nrd::SetCommonSettings(*instancecopy,settings);
+	frameprep(0, instancecopy);
 }
 
 void endDenoise(struct nrd::Instance* instanceinfo) {
 	nrd::Instance* instancecopy = instanceinfo;
 	//DestroyInstance(*instancecopy);
 }
-void frameprep(int framePosition) {
-
+void frameprep(int framePosition, nrd::Instance* instancecopy) {
+	//Need IMGUI setup to continue
 }
