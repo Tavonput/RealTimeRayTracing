fb77873552bcc6c2761566424a7ff49e2a9eb676
JohnnyFlint <115769806+JohnnyFlint@users.noreply.github.com>
2024-02-16 12:49:54 -0800

    Update frameprep function to fix compile error

--- RayTrace/Src/Application/simpleDenoise.cpp
+++ RayTrace/Src/Application/simpleDenoise.cpp
@@ -4,22 +4,24 @@
 #include "NRD.h"
 #include "NRDDescs.h"
 
-
-void startDenoise(struct nrd::InstanceCreationDesc& creationDetails,  struct nrd::Instance* instanceinfo,struct nrd::CommonSettings& settings,bool debug) {
-	nrd::Instance* instancecopy = instanceinfo;
-	if (debug) {
-		settings.debug = 1;
+namespace denoise {
+	void frameprep(int framePosition, nrd::Instance* instancecopy);
+	void startDenoise(struct nrd::InstanceCreationDesc& creationDetails, struct nrd::Instance* instanceinfo, struct nrd::CommonSettings& settings, bool debug) {
+		nrd::Instance* instancecopy = instanceinfo;
+		if (debug) {
+			settings.debug = 1;
+		}
+		//TODO: Fix repo to include more of NRD source files without having them on the realTimeRayTrace repo
+		 //nrd::CreateInstance(creationDetails, instancecopy);
+		 //nrd::SetCommonSettings(*instancecopy,settings);
+		denoise::frameprep(0, instancecopy);
 	}
-	//TODO: Fix repo to include more of NRD source files without having them on the realTimeRayTrace repo
-	 //nrd::CreateInstance(creationDetails, instancecopy);
-	 //nrd::SetCommonSettings(*instancecopy,settings);
-	frameprep(0, instancecopy);
-}
 
-void endDenoise(struct nrd::Instance* instanceinfo) {
-	nrd::Instance* instancecopy = instanceinfo;
-	//DestroyInstance(*instancecopy);
-}
-void frameprep(int framePosition, nrd::Instance* instancecopy) {
-	//Need IMGUI setup to continue
+	void endDenoise(struct nrd::Instance* instanceinfo) {
+		nrd::Instance* instancecopy = instanceinfo;
+		//DestroyInstance(*instancecopy);
+	}
+	void frameprep(int framePosition, nrd::Instance* instancecopy) {
+		//Need IMGUI setup to continue
+	}
 }
