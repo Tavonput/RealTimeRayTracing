9582761d98ccde6acaf3c37546cac2b8b8be9a0c
Brandon Truong <111801001+BrandonTruong315@users.noreply.github.com>
2024-02-23 11:28:33 -0800

    Adding video.mp4 player and dark mode
    Basically for this, I just added a video player in our demo section and also a dark and light mode just because I thought it would look cool

--- Raytracing_wesbite/script.js
+++ Raytracing_wesbite/script.js
@@ -6,3 +6,25 @@ document.querySelector('.slider').addEventListener('input', (e) => {
   // then it update the container's '--position' variable based on slider value.
   container.style.setProperty('--position', `${e.target.value}%`);
 })
+
+// function to toggle dark mode
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+  
+  // save the user preference in a localStorage
+  if(body.classList.contains('dark-mode')) {
+      localStorage.setItem('darkMode', 'enabled');
+  } else {
+      localStorage.setItem('darkMode', 'disabled');
+  }
+}
+
+// checking for the saved user preference
+document.addEventListener('DOMContentLoaded', (event) => {
+  if(localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+  }
+});
+
+
